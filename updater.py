c51a94a6233db88499f1cf5d73fa93313a4e90fd
DaRealTrueBlue <ddd19496@gmail.com>
2024-01-22 20:47:55 +1100

    modified:   updater.py

--- updater.py
+++ updater.py
@@ -12,5 +12,5 @@ def updater(url):
         print("Error: %s" % e)
 if __name__ == "__main__":
     print("Updating latest version...")
-    website_url = "https://github.com/DaRealTrueBlue/BitX/releases/download/latest/main.py"
+    website_url = "https://raw.githubusercontent.com/DaRealTrueBlue/BitX/master/main.py"
     updater(website_url)
