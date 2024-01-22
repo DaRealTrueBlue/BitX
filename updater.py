7c7ed0cd0059ae36c4c0d3642effcd19a3b69721
DaRealTrueBlue <ddd19496@gmail.com>
2024-01-22 20:36:48 +1100

    new file:   main.py 	new file:   updater.py

---
+++ updater.py
@@ -0,0 +1,16 @@
+import requests
+import subprocess
+
+def updater(url):
+    try:
+        response = requests.get(url)
+        code = response.text
+        with open("main.py", "w") as file:
+            file.write(code)
+        subprocess.check_call(["python", "main.py"])
+    except Exception as e:
+        print("Error: %s" % e)
+if __name__ == "__main__":
+    print("Updating latest version...")
+    website_url = "/main.py"
+    updater(website_url)
